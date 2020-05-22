ecdb977bc42e5cde04a69d8ae4e0baafa023e334
John Mondragon <j-mondragon@live.com>
2020-05-22 11:59:35 +0900

    update paths and readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # JasmineTest
+
 Jasmine coverage
+
+## Setup
+
+Install packages
+run on terminal:
+`npm i` or `npm install`
+
+## Run npm Tests
+
+using jamine
+run on terminal:
+`npx jasmine` or `npm jasmine`
+
+using grunt
+run on terminal:
+`grunt`
+
+### Check Coverage
+
+run on terminal:
+`mpn test`
+or
+`nyc --reporter=html jasmine`
+
+check page at
+/coverage/index.html
+
+## Actual Page Implementation
+
+/src/duplicateSignups.html
+
+## Jasmine Runner in Browser
+
+/src/DuplicateSignupsSpecRunner.html
--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,9 @@
-var fs = require('fs');
-// Enter correct environment Sitecore Path to enable grunt-copy on watch
-var sitecorePath = 'C:/inetpub/wwwroot/tpdevcd/Website';
-var sitecorePathCM = 'C:/inetpub/wwwroot/tpdevcm/Website';
-
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jasmine: {
       test: {
-        src: ['src/jquery.js', 'src/**/*.src.js'],
+        src: ['src/js/jquery.js', 'src/js/*.src.js'],
         options: {
           specs: 'src/test/*Spec.js',
           helpers: 'spec/helpers/*Helper.js',
